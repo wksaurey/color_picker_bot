f6c3a0f0e401e6200e660e604a70db125d65a914
kolter <wksaurey@gmail.com>
2025-01-03 23:20:02 -0700

    initial commit

---
+++ color_picker_bot.py
@@ -0,0 +1,11 @@
+import praw
+
+reddit = praw.Reddit(
+        client_id="my client id", 
+        client_secret="my client secret",
+        password="my password",
+        user_agent="my user agent",
+        username="my username",
+)
+
+print(reddit.read_only)
